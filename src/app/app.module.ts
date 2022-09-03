b862c7fc4664634f5cf340e15cf33af9e6370dbf
muzzu33 <muzzu3331@gmail.com>
2022-09-03 17:27:30 +0530

    new component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HomeComponent } from './home/home.component'
+import { HomeComponent } from './home/home.component';
+import { Parent2Component } from './components/parent2/parent2.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    Parent2Component,
 
 
   ],
